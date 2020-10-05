4c95100889958bf8778300a5eb0396bd722ceac1
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-05 10:12:30 +0300

    User bean class implemented

---
+++ src/test/java/BeansTests/userBeanTests/EmployeeBeanGettersTests.java
@@ -0,0 +1,77 @@
+package BeansTests.userBeanTests;
+
+import beans.employees.User;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeBeanGettersTests {
+
+    @Test
+    public void testGetId() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field id = user.getClass().getDeclaredField("id");
+        id.setAccessible(true);
+        id.set(user, 1);
+
+        assertEquals("field wasn't retrieved properly", user.getId(), 1);
+    }
+
+    @Test
+    public void testGetLogin() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field login = user.getClass().getDeclaredField("login");
+        login.setAccessible(true);
+        login.set(user, "newUser");
+
+        assertEquals("field wasn't retrieved properly", user.getLogin(), "newUser");
+    }
+
+    @Test
+    public void testGetPassword() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field password = user.getClass().getDeclaredField("password");
+        password.setAccessible(true);
+        password.set(user, "1234");
+
+        assertEquals("field wasn't retrieved properly", user.getPassword(), "1234");
+    }
+
+    @Test
+    public void testGetEmail() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field email = user.getClass().getDeclaredField("email");
+        email.setAccessible(true);
+        email.set(user, "newUser@gmail.com");
+
+        assertEquals("field wasn't retrieved properly", user.getEmail(), "newUser@gmail.com");
+    }
+
+    @Test
+    public void testGetHash() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field hash = user.getClass().getDeclaredField("hash");
+        hash.setAccessible(true);
+        hash.set(user, "1234abcd");
+
+        assertEquals("field wasn't retrieved properly", user.getHash(), "1234abcd");
+    }
+
+    @Test
+    public void testIsActivated() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+        final Field activated = user.getClass().getDeclaredField("activated");
+        activated.setAccessible(true);
+        activated.set(user, true);
+
+        assertEquals("field wasn't retrieved properly", user.isActivated(), true);
+    }
+}
---
+++ src/test/java/BeansTests/userBeanTests/EmployeeBeanSettersTests.java
@@ -0,0 +1,83 @@
+package BeansTests.userBeanTests;
+
+import beans.employees.User;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+public class EmployeeBeanSettersTests {
+
+    @Test
+    public void testSetId() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setId(1);
+
+        final Field id = user.getClass().getDeclaredField("id");
+        id.setAccessible(true);
+        assertEquals("Fields doesn't match", id.get(user), 1);
+    }
+
+    @Test
+    public void testSetLogin() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setLogin("newUser");
+
+        final Field login = user.getClass().getDeclaredField("login");
+        login.setAccessible(true);
+        assertEquals("Fields doesn't match", login.get(user), "newUser");
+    }
+
+    @Test
+    public void testSetPassword() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setPassword("1234");
+
+        final Field password = user.getClass().getDeclaredField("password");
+        password.setAccessible(true);
+        assertEquals("Fields doesn't match", password.get(user), "1234");
+    }
+
+    @Test
+    public void testSetEmail() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setEmail("newUser@gmail.com");
+
+        final Field email = user.getClass().getDeclaredField("email");
+        email.setAccessible(true);
+        assertEquals("Fields doesn't match", email.get(user), "newUser@gmail.com");
+    }
+
+    @Test
+    public void testSetHash() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setHash("1234abcd");
+
+        final Field hash = user.getClass().getDeclaredField("hash");
+        hash.setAccessible(true);
+        assertEquals("Fields doesn't match", hash.get(user), "1234abcd");
+    }
+
+    @Test
+    public void testSetActivated() throws NoSuchFieldException, IllegalAccessException {
+
+        final User user = new User();
+
+        user.setActivated(true);
+
+        final Field activated = user.getClass().getDeclaredField("activated");
+        activated.setAccessible(true);
+        assertEquals("Fields doesn't match", activated.get(user), true);
+    }
+}
