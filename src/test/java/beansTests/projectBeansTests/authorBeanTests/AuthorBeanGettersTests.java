afd57f0e02189caa9bc8723f0a254cfae5742a01
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-12 21:45:43 +0300

    Author bean tests implemented

---
+++ src/test/java/beansTests/projectBeansTests/authorBeanTests/AuthorBeanGettersTests.java
@@ -0,0 +1,44 @@
+package beansTests.projectBeansTests.authorBeanTests;
+
+import beans.employees.project.Author;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+public class AuthorBeanGettersTests {
+
+    @Test
+    public void testGetId() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+        final Field id = author.getClass().getDeclaredField("id");
+        id.setAccessible(true);
+        id.set(author, 1);
+
+        assertEquals("field wasn't retrieved properly", author.getId(), 1);
+    }
+
+    @Test
+    public void testGetName() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+        final Field name = author.getClass().getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(author, "name");
+
+        assertEquals("field wasn't retrieved properly", author.getName(), "name");
+    }
+
+    @Test
+    public void testGetSurname() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+        final Field surname = author.getClass().getDeclaredField("surname");
+        surname.setAccessible(true);
+        surname.set(author, "surname");
+
+        assertEquals("field wasn't retrieved properly", author.getSurname(), "surname");
+    }
+}
---
+++ src/test/java/beansTests/projectBeansTests/authorBeanTests/AuthorBeanSettersTests.java
@@ -0,0 +1,47 @@
+package beansTests.projectBeansTests.authorBeanTests;
+
+import beans.employees.project.Author;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.assertEquals;
+
+public class AuthorBeanSettersTests {
+
+    @Test
+    public void testSetId() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+
+        author.setId(1);
+
+        final Field id = author.getClass().getDeclaredField("id");
+        id.setAccessible(true);
+        assertEquals("Fields doesn't match", id.get(author), 1);
+    }
+
+    @Test
+    public void testSetName() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+
+        author.setName("name");
+
+        final Field name = author.getClass().getDeclaredField("name");
+        name.setAccessible(true);
+        assertEquals("Fields doesn't match", "name", name.get(author));
+    }
+
+    @Test
+    public void testSetSurname() throws NoSuchFieldException, IllegalAccessException {
+
+        final Author author = new Author();
+
+        author.setSurname("surname");
+
+        final Field surname = author.getClass().getDeclaredField("surname");
+        surname.setAccessible(true);
+        assertEquals("Fields doesn't match", "surname", surname.get(author));
+    }
+}
