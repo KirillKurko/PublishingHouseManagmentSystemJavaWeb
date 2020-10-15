79ec1466b669bbb91bbd82fc70cd885e8647f26c
Кирилл Курко <kirillkurko2609@gmail.com>
2020-10-15 22:37:45 +0300

    updateEmployee method implemented

--- src/main/java/dao/userDAOs/implementation/EmployeeDAOImplementation.java
+++ src/main/java/dao/userDAOs/implementation/EmployeeDAOImplementation.java
@@ -12,9 +12,9 @@ import java.sql.SQLException;
 public class EmployeeDAOImplementation implements EmployeeDAO {
 
     private static final String INSERT_EMPLOYEE = "INSERT INTO Employee(name, surname, experience, salary, userID) VALUES (?, ?, ?, ?, ?);";
-    private static final String UPDATE_USER = "UPDATE Employee SET name = ?, surname = ?, experience = ?, salary = ?, userID = ? WHERE id = ?;";
-    private static final String SELECT_USER = "SELECT * FROM Employee WHERE id = ?;";
-    private static final String DELETE_USER = "DELETE FROM Employee WHERE id = ?;";
+    private static final String UPDATE_EMPLOYEE = "UPDATE Employee SET name = ?, surname = ?, experience = ?, salary = ?, userID = ? WHERE id = ?;";
+    private static final String SELECT_EMPLOYEE = "SELECT * FROM Employee WHERE id = ?;";
+    private static final String DELETE_EMPLOYEE = "DELETE FROM Employee WHERE id = ?;";
 
     @Override
     public void insertEmployee(Employee employee) {
@@ -34,7 +34,21 @@ public class EmployeeDAOImplementation implements EmployeeDAO {
 
     @Override
     public boolean updateEmployee(Employee employee) {
-        return false;
+        boolean rowUpdated = false;
+        try (Connection connection = DatabaseUtility.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_EMPLOYEE)) {
+            preparedStatement.setString(1, employee.getName());
+            preparedStatement.setString(2, employee.getSurname());
+            preparedStatement.setInt(3, employee.getExperience());
+            preparedStatement.setDouble(4, employee.getSalary());
+            preparedStatement.setInt(5, employee.getUserID());
+            preparedStatement.setInt(6, employee.getId());
+            rowUpdated = preparedStatement.executeUpdate() > 0;
+        }
+        catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return rowUpdated;
     }
 
     @Override
